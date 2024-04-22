235038a6ef93dc68707ee2173c00b0cffbc9a6eb
beagas <beatrice.gaskiene@gmail.com>
2024-04-22 20:25:05 +0300

    Initial commit

---
+++ src/main/java/org/example/Plot/Plot.java
@@ -0,0 +1,24 @@
+package org.example.Plot;
+
+public class Plot {
+    String region;
+    String district;
+    String quartal;
+    String street;
+    String address;
+    String rcNo;
+
+    public void fillAD(){
+
+    }
+
+    public void fillAddress(){
+
+    }
+
+    public void fillRcNo(){
+
+    }
+
+
+}
---
+++ src/test/java/PlotsTests.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class PlotsTests {
+    public static WebDriver _driver;
+    public static WebDriverWait _wait;
+}
