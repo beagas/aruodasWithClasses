97fe5c68fbf5b9f1cb3045081e0af973b9f7ca94
beagas <beatrice.gaskiene@gmail.com>
2024-04-22 21:31:32 +0300

    SetupDriver

--- src/test/java/PlotsTests.java
+++ src/test/java/PlotsTests.java
@@ -14,4 +14,20 @@ import java.util.concurrent.TimeUnit;
 public class PlotsTests {
     public static WebDriver _driver;
     public static WebDriverWait _wait;
+
+    @BeforeClass
+    public void SetupWebDriver() {
+        ChromeOptions options = new ChromeOptions();
+        _driver = new ChromeDriver();
+        options.addArguments("--start-maximized");
+        _driver.get("https://www.saucedemo.com/");
+        _driver.findElement(By.id("user-name")).sendKeys("problem_user");
+        _driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        _driver.findElement(By.id("login-button")).click();
+    }
+
+
+
+
+
 }
